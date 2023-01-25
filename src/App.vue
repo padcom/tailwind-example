11ea303079a1e583dbb330fbf34ab111c5cc5093
Matthias Hryniszak <padcom@gmail.com>
2023-01-25 22:43:43 +0100

    Added comparison of the built files when the tailwindcss code is inlined vs used in classes with @apply

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="header">{{ date.now.toLocaleTimeString() }}</header>
-  <div class="container">
-    <button @click="date.sayHello('John')" class="hello">Say hello!</button>
+  <header class="text-blue-900 border border-solid border-red-500 text-9xl grid place-items-center mb-10">{{ date.now.toLocaleTimeString() }}</header>
+  <div class="px-[25%] grid">
+    <button @click="date.sayHello('John')" class="font-bold py-2 px-4 rounded bg-blue-500 text-white hover:bg-blue-700">Say hello!</button>
     <Example />
   </div>
 </template>
@@ -12,33 +12,3 @@ import Example from './components/Example.vue'
 import { useDateStore } from './stores/date'
 const date = useDateStore()
 </script>
-
-<style lang="postcss" scoped>
-header {
-  @apply text-blue-900 border border-solid border-red-500;
-}
-
-.header {
-  @apply text-9xl;
-  @apply grid place-items-center;
-  @apply mb-10;
-}
-
-.container {
-  @apply px-[25%];
-  @apply grid;
-}
-
-.hello {
-  @apply font-bold py-2 px-4 rounded;
-  @apply bg-blue-500 text-white;
-}
-.hello:hover {
-  @apply bg-blue-700;
-}
-
-/* This class will NOT be included */
-.app-scoped {
-  @apply text-blue-900 border border-solid border-red-500;
-}
-</style>
